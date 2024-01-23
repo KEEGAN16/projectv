77503517e974ab51d974a81c7273b7158f6b5490
KEEGAN16 <dmitrenkovlad16@gmail.com>
2024-01-24 00:10:31 +0100

    projectv
    projectv

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  totalPrice: Number
+})
+
+const emit = defineEmits(['openDrawer'])
+</script>
+
+<template>
+  <header class="flex justify-between border-b border-slate-200 px-10 py-8">
+    <router-link to="/"
+      ><div class="flex items-center">
+        <img src="/logo2.png" alt="Logo" class="w-20 mr-5" />
+        <div>
+          <p class="text-slate-400">Obchod s najlepšími teniskami</p>
+        </div>
+      </div></router-link
+    >
+
+    <ul class="flex items-center gap-10">
+      <li
+        @click="() => emit('openDrawer')"
+        class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black"
+      >
+        <img src="/cart.svg" alt="Cart" />
+        <b>{{ totalPrice }} eur.</b>
+      </li>
+
+      <router-link to="/favorites">
+        <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+          <img src="/heart.svg" alt="Cart" />
+          <span>Záložky</span>
+        </li></router-link
+      >
+      <router-link to="/registration">
+        <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+          <img src="/profile.svg" alt="Cart" />
+          <span>Profil</span>
+        </li></router-link
+      >
+    </ul>
+  </header>
+</template>
---
+++ src/pages/Registration.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="registration-form">
+    <h2 class="heading">Registrácia</h2>
+
+    <form @submit.prevent="submitForm">
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="email" required class="form-control" />
+      </div>
+
+      <div class="form-group">
+        <label for="password">Heslo:</label>
+        <input type="password" id="password" v-model="password" required class="form-control" />
+      </div>
+
+      <div class="form-group">
+        <label for="firstName">Krstné meno:</label>
+        <input type="text" id="firstName" v-model="firstName" required class="form-control" />
+      </div>
+
+      <div class="form-group">
+        <label for="lastName">Priezvisko:</label>
+        <input type="text" id="lastName" v-model="lastName" required class="form-control" />
+      </div>
+
+      <button type="submit" class="btn btn-primary">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      firstName: '',
+      lastName: ''
+    }
+  },
+  methods: {
+    submitForm() {
+      console.log('Registration:', this.email, this.password, this.firstName, this.lastName)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.registration-form {
+  background-color: #fff;
+  padding: 2rem;
+  border-radius: 5px;
+  margin: 0 auto;
+  width: 500px;
+}
+
+.heading {
+  text-align: center;
+  margin-bottom: 20px;
+  font-size: 2em;
+  font-weight: 700;
+}
+
+.form-group {
+  margin-bottom: 10px;
+}
+
+.form-control {
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+}
+
+.btn {
+  padding: 10px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+}
+</style>
